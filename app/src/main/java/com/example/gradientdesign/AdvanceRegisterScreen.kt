118c4d5db1d99823dcc260da1bcb94b2120d1915
Shivangi <shivangit1111@gmail.com>
2023-03-20 14:52:25 +0530

    chore: add new activity

---
+++ app/src/main/java/com/example/gradientdesign/AdvanceRegisterScreen.kt
@@ -0,0 +1,11 @@
+package com.example.gradientdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdvanceRegisterScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_advance_register_screen)
+    }
+}
