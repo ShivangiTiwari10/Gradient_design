9ec6c0979424666af50515ebf050ad68bebb6b38
Shivangi <shivangit1111@gmail.com>
2023-03-21 12:29:13 +0530

    feat: registered user,send to login screen

--- app/src/main/java/com/example/gradientdesign/AdvanceRegisterScreen.kt
+++ app/src/main/java/com/example/gradientdesign/AdvanceRegisterScreen.kt
@@ -1,16 +1,44 @@
 package com.example.gradientdesign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.gradientdesign.databinding.ActivityAdvanceRegisterScreenBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class AdvanceRegisterScreen : AppCompatActivity() {
-
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityAdvanceRegisterScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityAdvanceRegisterScreenBinding.inflate(layoutInflater)
+
+        auth = FirebaseAuth.getInstance()
+
         setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            login()
+        }
+
+        binding.tvRedirectSignUp.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            // using finish() to end the activity
+            finish()
+        }
+    }
+
+    private fun login() {
+        val email = binding.etEmailAddress.text.toString()
+        val pass = binding.etPassword.text.toString()
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Successfully LoggedIn", Toast.LENGTH_SHORT).show()
+            } else
+                Toast.makeText(this, "Log In failed ", Toast.LENGTH_SHORT).show()
+        }
     }
 }
