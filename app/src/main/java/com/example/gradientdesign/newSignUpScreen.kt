9ad3b75592c255940a697725dc0e526dc596785b
Shivangi <shivangit1111@gmail.com>
2023-03-21 13:42:23 +0530

    chore: add new activity

---
+++ app/src/main/java/com/example/gradientdesign/newSignUpScreen.kt
@@ -0,0 +1,11 @@
+package com.example.gradientdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class newSignUpScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_sign_up_screen)
+    }
+}
