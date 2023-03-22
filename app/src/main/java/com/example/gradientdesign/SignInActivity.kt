19b11b05f761c229df56256b1268e2158506c580
Shivangi <shivangit1111@gmail.com>
2023-03-22 11:29:53 +0530

    feat: Bind view

--- app/src/main/java/com/example/gradientdesign/SignInActivity.kt
+++ app/src/main/java/com/example/gradientdesign/SignInActivity.kt
@@ -2,10 +2,14 @@ package com.example.gradientdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gradientdesign.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
