841b6a2ec8dee661330073ec36c689794fb3d7c5
Shivangi <shivangit1111@gmail.com>
2023-03-24 10:38:19 +0530

    feat/bind view

--- app/src/main/java/com/example/gradientdesign/WelcomeActivity.kt
+++ app/src/main/java/com/example/gradientdesign/WelcomeActivity.kt
@@ -2,10 +2,16 @@ package com.example.gradientdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gradientdesign.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var binding
+            : ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
