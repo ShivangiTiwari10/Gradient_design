7963efd14a124fb9b5d3889be9239b0ffdfb8baa
Shivangi <shivangit1111@gmail.com>
2023-03-24 13:30:06 +0530

    feat: get data

--- app/src/main/java/com/example/gradientdesign/WelcomeActivity.kt
+++ app/src/main/java/com/example/gradientdesign/WelcomeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gradientdesign
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gradientdesign.databinding.ActivityWelcomeBinding
@@ -8,10 +9,21 @@ class WelcomeActivity : AppCompatActivity() {
     private lateinit var binding
             : ActivityWelcomeBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val name = intent.getStringExtra(SignInActivity.KEY2)
+        val email = intent.getStringExtra(SignInActivity.KEY1)
+        val id = intent.getStringExtra(SignInActivity.KEY3)
+
+
+        binding.tvWelcome.text = "Name: $name"
+        binding.tvMail.text = "Email: $email"
+        binding.tvUnique.text = "UniqueId: $id"
+
     }
 }
